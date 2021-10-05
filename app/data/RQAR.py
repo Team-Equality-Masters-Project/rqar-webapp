f9807b35edd4ce81a848ab6e9d65eedc9f956863
Haley <haleyhfeng@gmail.com>
2021-10-04 18:41:03 -0700

    Update bert and faiss
    bert to semantic search trained

--- app/data/RQAR.py
+++ app/data/RQAR.py
@@ -9,8 +9,8 @@ import faiss
 # Load final train dataset
 df_QA = pd.read_csv('data/dataset/main_data.csv')
 corpus = list(np.unique(df_QA['question_vocab'].tolist()))
-# Load BERT
-embedder = SentenceTransformer('bert-base-nli-mean-tokens')
+# Load Semantic Search Trained BERT
+embedder = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
 # Load Faiss
 with open('data/faiss_index.pickle', "rb") as h:
      index = faiss.deserialize_index(pickle.load(h))
@@ -19,20 +19,21 @@ with open('data/faiss_index.pickle', "rb") as h:
 def RQAR(new_question):
 
      num_rec = 10 # Number of related questions/neighbors to search for with Faiss
+     question_type = 'question_vocab' # Using cleaned historical question
 
      # 0. Preprocess question and add topic inference
      new_question = normalize_text(new_question)
 
      # 1. Encode question and find relevant doc
      quest_embed = embedder.encode(new_question)
-     distances, indices = index.search(np.asarray(quest_embed).reshape(1,768),num_rec)
+     distances, indices = index.search(np.asarray(quest_embed).reshape(1,quest_embed.shape[0]),num_rec)
      L2_distance = distances[0] 
      norm_L2_distance = L2_distance/np.linalg.norm(L2_distance) # scale distance to [0, 1]
      relevant_docs = [corpus[indices[0,idx]] for idx in range(num_rec)]
 
      # 2. Create a new dataframe for suggestion
      relevant = pd.DataFrame({'relevant_question':relevant_docs, 'semantic_similarity':norm_L2_distance})
-     relevant_subreddit = relevant.merge(df_QA, left_on='relevant_question', right_on='question_vocab', how='inner')
+     relevant_subreddit = relevant.merge(df_QA, left_on='relevant_question', right_on=question_type, how='inner')
      relevant_subreddit = relevant_subreddit[['relevant_question','suggested_subreddits', 'semantic_similarity', 'comment_upvotes']]
 
      # 3. Calculate total weight by combining upvotes, occurrences, similarity
